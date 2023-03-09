0acfa6c04d80238781ecb6b04a32ed368080dca8
AshishSharma03 <30octaksharma@gmail.com>
2023-03-10 04:34:47 +0530

    mlSolution

--- pages/Screens/MLCameraScreen.js
+++ pages/Screens/MLCameraScreen.js
@@ -1,25 +1,32 @@
+
 import { Box } from '@mui/material'
 import React,{useEffect,useRef,useState} from 'react'
 
+
+
+
 function MLCameraScreen() {
     const videoRef = useRef(); 
-    const [Loaded, setLoaded] = useState(false); 
-        useEffect(() => {
-            navigator.mediaDevices
-            .getUserMedia({ video: {facingMode:{exact:"environment"}}, audio: false })
-            .then((stream) => {
-              videoRef.current.srcObject = stream;
-              videoRef.current.play();
-              setLoaded(true);
-            });
-        }, []);
+    // const [Loaded, setLoaded] = useState(false); 
+    //     useEffect(() => {
+    //             navigator.mediaDevices
+    //             // .getUserMedia({ video: {facingMode:{exact:"environment"}}, audio: false })
+    //             .getUserMedia({ video: true, audio: false })
+    //             .then((stream) => {
+    //                 videoRef.current.srcObject = stream;
+    //                 videoRef.current.play();
+    //                 setLoaded(true);
+    //             });
+            
+    //     }, []);
 
   return (
     <Box display={'flex'} justifyContent="center" alignItems="center"  minHeight="80vh">
-        <video
-        style={{position:"fixed"}}
-        ref={videoRef}
+        {/* <WebView source={{uri:"https://mlbizuka-backup.vercel.app/"}} /> */}
+        <iframe src='https://mlbizuka-backup.vercel.app/' allow='camera'
+                style={{height:"80vh",width:"100%",border:"none"}}
         />
+
     </Box>
   )
 }
