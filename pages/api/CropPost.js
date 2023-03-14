b462597cc60dee9fefa1f34545d0191615941c52
AshishSharma03 <30octaksharma@gmail.com>
2023-03-14 19:52:38 +0530

    DbConnect_CreateCropPostRequest

--- pages/api/CropPost.js
+++ pages/api/CropPost.js
@@ -1,6 +1,30 @@
+import dbConnect from "../../lib/dbConnect";
+import cropModel from "../../lib/model/cropModel";
 
+export default async function handler(req, res) {
+     const { method } = req
+    await dbConnect()
+    switch(method){
+      case 'GET' : 
 
-export default function handler(req, res) {
-    res.status(200).json({ name: 'John Doe' })
+      try{
+        const CropPost = await cropModel.find({})
+        res.status(200).json({success : true , data : CropPost})
+      }catch(error){
+        res.status(400).json({success:false})
+      }
+
+    break
+      case 'POST' :
+      try{
+          const NewPost = await cropModel.create(req.body)
+          res.status(200).json({success :true,data: NewPost})
+      }catch(error){
+          res.status(400).json({ success: false })
+      }
+    
+    break
+    default :  
+    }
   }
   
